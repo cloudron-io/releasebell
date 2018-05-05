e982268c7c1b0f89a8101196a3caa99675a6e4b3
Johannes Zellner <johannes@nebulon.de>
2018-05-05 18:39:19 +0200

    Cloudronize the app

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM cloudron/base:0.10.0
+
+ENV PATH /usr/local/node-6.9.5/bin:$PATH
+
+RUN mkdir -p /app/code/
+WORKDIR /app/code
+
+RUN ln -s /run/database.json /app/code/database.json
+
+ADD backend /app/code/backend
+ADD frontend /app/code/frontend
+ADD package.json index.js start.sh /app/code/
+
+RUN npm install --production
+
+CMD [ "/app/code/start.sh" ]
---
+++ start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -eu
+
+export NODE_ENV=production
+
+echo "=> Create database.json"
+cat <<EOF > /run/database.json
+{
+    "defaultEnv": "cloudron",
+    "cloudron": {
+        "host": "${MYSQL_HOST}",
+        "port": ${MYSQL_PORT}
+        "user": "${MYSQL_USERNAME}",
+        "password": "${MYSQL_PASSWORD}",
+        "database": "${MYSQL_DATABASE}",
+        "driver": "mysql",
+        "multipleStatements": true
+    }
+}
+EOF
+
+echo "=> Run db-migration"
+/app/code/node_modules/bin/db-migration up
+
+echo "=> Start application"
+exec /usr/local/bin/gosu cloudron:cloudron node /app/code/index.js
