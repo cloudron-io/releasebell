e016152c886e303d6665efa54b91f0ad94ee0762
Johannes Zellner <johannes@nebulon.de>
2018-05-05 18:48:40 +0200

    Fixup the cloudron package code

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ RUN ln -s /run/database.json /app/code/database.json
 
 ADD backend /app/code/backend
 ADD frontend /app/code/frontend
+ADD migrations /app/code/migrations
 ADD package.json index.js start.sh /app/code/
 
 RUN npm install --production
--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ cat <<EOF > /run/database.json
     "defaultEnv": "cloudron",
     "cloudron": {
         "host": "${MYSQL_HOST}",
-        "port": ${MYSQL_PORT}
+        "port": ${MYSQL_PORT},
         "user": "${MYSQL_USERNAME}",
         "password": "${MYSQL_PASSWORD}",
         "database": "${MYSQL_DATABASE}",
@@ -21,7 +21,7 @@ cat <<EOF > /run/database.json
 EOF
 
 echo "=> Run db-migration"
-/app/code/node_modules/bin/db-migration up
+/app/code/node_modules/.bin/db-migrate up
 
 echo "=> Start application"
 exec /usr/local/bin/gosu cloudron:cloudron node /app/code/index.js
