921a1237af4e11efc0870c3c141a824c45ea5fd1
Johannes Zellner <johannes@nebulon.de>
2018-05-08 00:29:53 +0200

    Kick off tasks only after the server started

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ var server = require('./backend/server.js'),
 database.init(function (error) {
     if (error) return console.error('Failed to init database.', error);
 
-    tasks.run();
-
     server.start(function (error) {
         if (error) return console.error('Failed to start server.', error);
 
         console.log('Server is up and running.');
+
+        tasks.run();
     });
 });
