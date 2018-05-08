46b1fbdc3505954fd1348ef6fd27d52f67bc8739
Johannes Zellner <johannes@nebulon.de>
2018-05-08 18:30:49 +0200

    Show list of last releases of all projects if a user has tracked projects

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -86,7 +86,7 @@ new Vue({
             if (!this.profile) this.activeView = 'login';
             else this.activeView = key;
 
-            if (this.activeView === 'projects') {
+            if (this.activeView === 'projects' || this.activeView === 'welcome') {
                 this.projects = null;
 
                 superagent.get('/api/v1/projects').auth(this.login.username, this.login.password).end(function (error, result) {
@@ -99,6 +99,26 @@ new Vue({
                 this.login.username = '';
                 this.login.password = '';
             }
+        },
+        prettyDate: function (row, column, cellValue, index) {
+            var date = new Date(cellValue),
+            diff = (((new Date()).getTime() - date.getTime()) / 1000),
+            day_diff = Math.floor(diff / 86400);
+
+            if (isNaN(day_diff) || day_diff < 0)
+                return;
+
+            return day_diff === 0 && (
+                diff < 60 && 'just now' ||
+                diff < 120 && '1 minute ago' ||
+                diff < 3600 && Math.floor( diff / 60 ) + ' minutes ago' ||
+                diff < 7200 && '1 hour ago' ||
+                diff < 86400 && Math.floor( diff / 3600 ) + ' hours ago') ||
+                day_diff === 1 && 'Yesterday' ||
+                day_diff < 7 && day_diff + ' days ago' ||
+                day_diff < 31 && Math.ceil( day_diff / 7 ) + ' weeks ago' ||
+                day_diff < 365 && Math.round( day_diff / 30 ) +  ' months ago' ||
+                Math.round( day_diff / 365 ) + ' years ago';
         },
         setProjectState: function (projectId, state, scope) {
             var that = this;
